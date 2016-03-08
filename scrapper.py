de26e4f9b821b31fa14217bc0b076281ba0bcf9b
w.souza <w.souza@fliegersoftware.de>
2016-03-08 16:27:18 -0300

    Importar módulo de log e adicionar log de teste

--- scrapper.py
+++ scrapper.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from modules.logger import Logger as log
+
 class Scrapper(object):
 	def __init__(self):
 		super(Scrapper, self).__init__()
 
 	def start(self):
-		pass
+		log.warning('ok')
 		
 if __name__ == '__main__':
 	scrapper = Scrapper()
