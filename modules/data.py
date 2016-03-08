4282334b18d5b459927465ea5256cde9ae4f2a50
w.souza <w.souza@fliegersoftware.de>
2016-03-08 17:26:41 -0300

    Inicio do módulo de data

--- modules/data.py
+++ modules/data.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from modules.generator import Generator as generator
+
+import os
+
+
 class Data(object):
 	def __init__(self):
 		super(Data, self).__init__()
+
+
+	@staticmethod
+	def get(item):
+		data = Data
+		if item == 'notices.list':
+			data.getNotices()
+
+	def getNotices():
+		noticesFile = 'data/notices.json'
+
+		if os.path.isfile(noticesFile):
+			return open(noticesFile, 'r').read()
+		else:
+			generator.setNotices()
