7d1b17bdffddd6453ef2d8605882f66b2724a818
w.souza <w.souza@fliegersoftware.de>
2016-03-08 17:00:46 -0300

    Remover logs de teste

--- scrapper.py
+++ scrapper.py
@@ -3,15 +3,16 @@
 
 from modules.logger import Logger as log
 
+
 class Scrapper(object):
 	def __init__(self):
 		super(Scrapper, self).__init__()
 
+
 	def start(self):
-		log.success('sucesso')
-		log.error('erro')
-		log.warning('warning')
-		
+		pass
+
+
 if __name__ == '__main__':
 	scrapper = Scrapper()
 	scrapper.start()
