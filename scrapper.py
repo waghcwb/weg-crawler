6a1e1f2a404bab80c806e33ade719b889644f83e
w.souza <w.souza@fliegersoftware.de>
2016-03-08 16:17:05 -0300

    Adicionar chamada para a função principal

--- scrapper.py
+++ scrapper.py
@@ -3,3 +3,11 @@
 class Scrapper(object):
 	def __init__(self):
 		super(Scrapper, self).__init__()
+
+	def start(self):
+		pass
+
+
+if __name__ == '__main__':
+	scrapper = Scrapper()
+	scrapper.start()
