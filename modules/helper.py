fe8496f1645091c6cafc5245249f5a80f6b222d3
w.souza <w.souza@fliegersoftware.de>
2016-03-08 17:00:02 -0300

    Adicionar módulo helper

---
+++ modules/helper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+
+class Helper(object):
+	def __init__(self):
+		super(Helper, self).__init__()
+
+
+	@staticmethod
+	def createFile(filename, content, mode='a+'):
+		os.chdir(sys.path[0])
+
+		with open(filename, mode) as file:
+		    file.write(content)
