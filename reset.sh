cf2d44d3fcad5cba9a15d1d0924b6b104a381eea
w.souza <w.souza@fliegersoftware.de>
2016-03-09 19:22:53 -0300

    Remover JSON de imagens no reset

--- reset.sh
+++ reset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf data/images/notices/* data/images/images.json data/notices/impex/* data/notices/dump.json data/notices.json logs/tables/* logs/tags/*
+rm -rf data/images/notices/* data/images.json data/notices/impex/* data/notices/dump.json data/notices.json logs/tables/* logs/tags/*
