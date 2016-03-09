8cad1cee95e2de34012c8d291a985f8369581961
w.souza <w.souza@fliegersoftware.de>
2016-03-09 15:29:35 -0300

    módulos iniciais do crawler

--- modules/crawler.py
+++ modules/crawler.py
@@ -12,5 +12,16 @@ class Crawler(object):
 
 
 	@staticmethod
-	def request(url, parser='html.parser'):
-		return BeautifulSoup(requests.get(url).text, parser)
+	def getData(nid, category, language, catalog, link, parser='html.parser'):
+		html = BeautifulSoup(requests.get(link).text, parser)
+		return Crawler.parseData(html, nid, category, language, catalog, link)
+
+	@staticmethod
+	def parseData(document, nid, category, language, catalog, link):
+		title       = document.select('.noticia-titulo h1')
+		subtitle    = document.select('.noticia-titulo h2')
+		tags        = document.select('.tags a')
+		banner      = document.select('.imagem-corpo-noticia img')[0].get('src') if document.select('.imagem-corpo-noticia img') else 'empty'
+		content     = html.escape(str(document.select('.coluna5 .texto')[0])).encode('ascii', 'xmlcharrefreplace').decode()
+		timestamp   = document.select('.noticia-detalhe .data')[0].attrs['date-timestamp']
+		publishDate = datetime.fromtimestamp(int(timestamp)).strftime('%d/%m/%Y')
