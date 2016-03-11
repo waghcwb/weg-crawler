5b40a191469b5d1cc3a686cda1653ad9dc07cc9e
w.souza <w.souza@fliegersoftware.de>
2016-03-11 16:55:20 -0300

    Ajustes para o helper criar arquivos JSON

--- modules/helper.py
+++ modules/helper.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from modules.logger import Logger as log
+
 import sys
 import os
+import json
 
 
 class Helper(object):
@@ -11,8 +14,15 @@ class Helper(object):
 
 
 	@staticmethod
-	def createFile(filename, content, mode='a+'):
+	def createFile(filename, content, mode='a+', format=None):
 		os.chdir(sys.path[0])
 
 		with open(filename, mode, encoding='utf-8') as file:
-		    file.write(content)
+			if format == 'json':
+				try:
+					file.write(json.dumps(content, indent=4, sort_keys=True))
+				except Exception as error:
+					log.error(error.args[0])
+					pass
+			else:
+				file.write(content)
