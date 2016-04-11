6af0c907b48ca4a8828b0694eb4d696dbff913d5
w.souza <w.souza@fliegersoftware.de>
2016-04-11 13:53:25 -0300

    deixar script de reset mais legível

--- reset.sh
+++ reset.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-rm -rf data/news/images/* data/images.json data/news/impex/* data/news/dump.json data/notices.json logs/tables/* logs/tags/*
+folders_to_remove=(
+	'data/news/images/*'
+	'data/images.json'
+	'data/news/impex/*'
+	'data/news/dump.json'
+	'data/notices.json'
+	'logs/tables/*'
+	'logs/tags/*'
+)
+
+x=0;
+
+while [ $x != ${#folders_to_remove[@]} ]
+do
+	folder=${folders_to_remove[$x]}
+
+	echo "Pasta apagada: ${folder}"
+
+	rm -rf ${folder}
+
+	let "x = x +1"
+done
