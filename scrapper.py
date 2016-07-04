a1320b65c80240c4e83afc99d7e4aaf711f9b131
Wagner Souza <wagner.junior@fh.com.br>
2016-07-04 10:39:18 -0300

    tirar tempo entre as requisições

--- scrapper.py
+++ scrapper.py
@@ -56,7 +56,7 @@ class Scrapper(object):
 						pass
 					finally:
 						helper.createFile('data/notices.json', json.dumps(noticesList, indent=4, sort_keys=True), mode='w')
-						time.sleep(20)
+						time.sleep(3)
 			else:
 				log.warning('Dados dessa notícia já foram adquiridos [{nid}]'.format(nid=nid))
 
